e953aba47895763784e702766a4a2df0ebc10d0e
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-04-23 20:48:50 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./Components/Pages/Home";
+
+function App() {
+  return (
+    <div className="max-w-md mx-auto">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
